ac18c57a1abe3c83026ff873af6d170983c16681
Jacek Oleksy <jacekoleksy99@gmail.com>
2022-10-22 14:37:07 +0200

    Delete dbconfig.php

--- dbconfig.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-const USERNAME = 'thutemsxkjvtqn';
-const PASSWORD = 'd7ca0d47ad908d7d461214722537f804cc63de5e8ff6d9f6cb379cca5e86df11';
-const HOST = 'ec2-3-248-121-12.eu-west-1.compute.amazonaws.com';
-const DATABASE = 'd525mpd91ut3hs';
-
-?>
